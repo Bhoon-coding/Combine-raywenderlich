c85824b65e87eedaa8ec7624e7f2485c0e2c7632
bhoon-coding <bhooncoding@gmail.com>
2022-10-28 00:07:16 +0900

    [Combine] Chapter 3: Challenge done

--- 03-transforming-operators/projects/challenge/Starter.playground/Contents.swift
+++ 03-transforming-operators/projects/challenge/Starter.playground/Contents.swift
@@ -10,7 +10,7 @@ example(of: "Create a phone number lookup") {
     "217-555-1212": "Scott",
     "212-555-3434": "Shai"
   ]
-  
+
   func convert(phoneNumber: String) -> Int? {
     if let number = Int(phoneNumber),
       number < 10 {
@@ -47,7 +47,7 @@ example(of: "Create a phone number lookup") {
 
     return phone
   }
-
+  
   func dial(phoneNumber: String) -> String {
     guard let contact = contacts[phoneNumber] else {
       return "Contact not found for \(phoneNumber)"
@@ -57,8 +57,15 @@ example(of: "Create a phone number lookup") {
   }
   
   let input = PassthroughSubject<String, Never>()
-  
-  <#Add your code here#>
+
+    input
+        .map(convert) // String -> Int 변환 >> (문자입력시) keyMap을 통해 문자에 포함되는 숫자로 변환
+        .replaceNil(with: 0) // Optional 바인딩 처리 후, 변환할 수 없는것 (nil) -> 0으로 대체
+        .collect(10) // 한 배열에 10개 요소 담기
+        .map(format) // Int -> String >> String 통합 >> 인덱스 3번째, 7번째 자리에 "-" 추가
+        .map(dial) // contacts[phoneNumber] 키 값으로 접근해 value(사람이름) 반환
+        .sink(receiveCompletion: { print($0) },
+              receiveValue: { print($0) })
   
   "0!1234567".forEach {
     input.send(String($0))
@@ -72,35 +79,3 @@ example(of: "Create a phone number lookup") {
     input.send("\($0)")
   }
 }
-
-/// Copyright (c) 2021 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
