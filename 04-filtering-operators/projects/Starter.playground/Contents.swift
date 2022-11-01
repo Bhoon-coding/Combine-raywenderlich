3e9b636ca209893741cc336908b1967992830bde
bhoon-coding <bhooncoding@gmail.com>
2022-11-01 20:58:26 +0900

    [Combine] Chapter4: removeDuplicate(), compactMap()

--- 04-filtering-operators/projects/Starter.playground/Contents.swift
+++ 04-filtering-operators/projects/Starter.playground/Contents.swift
@@ -1,20 +1,23 @@
 import Foundation
 import Combine
 
-var subscriptions = Set<AnyCancellable>()
 
-example(of: "filter") {
-    // 1. 1~10까지 이벤트 방출
-    let numbers = (1...10).publisher
-    
-    // 2. filter 연산자로, 3의 배수만 걸러냄
-    numbers
-        .filter { $0.isMultiple(of: 3) }  // isMultiple of의 지정된 숫자의 배수이면 true 반환
-        .sink(receiveValue: { num in
-            print("\(num) 은 3의 배수")
-        })
-        .store(in: &subscriptions)
-}
+// TODO: [x] Filter
+
+//var subscriptions = Set<AnyCancellable>()
+//
+//example(of: "filter") {
+//    // 1. 1~10까지 이벤트 방출
+//    let numbers = (1...10).publisher
+//
+//    // 2. filter 연산자로, 3의 배수만 걸러냄
+//    numbers
+//        .filter { $0.isMultiple(of: 3) }  // isMultiple of의 지정된 숫자의 배수이면 true 반환
+//        .sink(receiveValue: { num in
+//            print("\(num) 은 3의 배수")
+//        })
+//        .store(in: &subscriptions)
+//}
 
 /*
  ——— Example of: filter ———
@@ -23,7 +26,55 @@ example(of: "filter") {
  9 은 3의 배수
 */
 
+// TODO: [x] removeDuplicates
 
+//var subscriptions = Set<AnyCancellable>()
+//
+//example(of: "removeDuplicate") {
+//    let words = "hey hey there! want to listen to mister mister ?"
+//        .components(separatedBy: " ") // " " 띄워쓰기 부분별로 나눠 배열에 담음
+//        // ["hey", "hey", "there!", "want", "to", "listen", "to", "mister", "mister", "?"]
+//        .publisher
+//
+//    words
+//        .removeDuplicates() // 중복된 요소가 있을 경우 하나만 방출합니다.
+//        .sink(receiveValue: { print($0) })
+//        .store(in: &subscriptions)
+//}
+
+/*
+ ——— Example of: removeDuplicate ———
+ hey
+ there!
+ want
+ to
+ listen
+ to
+ mister
+ ?
+*/
+
+// TODO: [x] compactMap
+
+var subscriptions = Set<AnyCancellable>()
+
+example(of: "compactMap") {
+    
+    let strings = ["a", "1.24", "3", "def", "45", "0.23"].publisher
+
+    strings
+        .compactMap { Float($0) }
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+/*
+ ——— Example of: compactMap ———
+ 1.24
+ 3.0
+ 45.0
+ 0.23
+*/
 
 /// Copyright (c) 2021 Razeware LLC
 ///
