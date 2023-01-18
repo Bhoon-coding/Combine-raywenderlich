57059ae6fba9b57852f9520059240e2f3884fba6
bhoon-coding <bhooncoding@gmail.com>
2023-01-18 10:15:32 +0900

    [Combine] Chapter13 : share()

--- 13-Resource Management/projects/Starter.playground/Pages/Timer.xcplaygroundpage/Contents.swift
+++ 13-Resource Management/projects/Starter.playground/Pages/Timer.xcplaygroundpage/Contents.swift
@@ -1,24 +1,29 @@
 import Combine
 import Foundation
 
-let main = Timer.publish(every: 1.0, on: .main, in: .common)
-let current = Timer.publish(every: 1.0, on: .current, in: .common)
+let shared = URLSession.shared
+    .dataTaskPublisher(for: URL(string: "https://www.raywenderlich.com")!)
+    .map(\.data)
+    .print("shared")
+    .share()
 
-let publisher = Timer
-    .publish(every: 1.0, on: .main, in: .common)
-    .autoconnect()
+print("subscribing first")
 
-let subscription = Timer
-    .publish(every: 1.0, on: .main, in: .common)
-    .autoconnect()
-    // 이전 값에 +1을 함 (reduce는 최종값만 나타내지만, scan은 지속적으로 값을 보여줌)
-    .scan(0) { counter, _ in
-        counter + 1
-    }
-    .sink { counter in
-        print("Counter is \(counter)")
-    }
-// Counter is 1
-// Counter is 2
-// Counter is 3
-// Counter is 4
+let subscription1 = shared
+    .sink(receiveCompletion: { _ in },
+          receiveValue: { print("subscription1 received: '\($0)'") }
+    )
+
+print("subscribing second")
+
+var subscription2: AnyCancellable? = nil
+    
+DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+    print("subscribing second")
+    
+    subscription2 = shared
+        .sink(
+            receiveCompletion: { print("subscription2 completion \($0)") },
+            receiveValue: { print("subscription2 received: '\($0)'") }
+        )
+}
