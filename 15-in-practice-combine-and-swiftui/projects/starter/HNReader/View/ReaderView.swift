4857ae10ba9883ee55b7a4ea07ed8452bdbb06b5
bhoon-coding <bhooncoding@gmail.com>
2023-02-08 09:23:42 +0900

    [Combine] Chapter 15: 컴파일 에러 문제 해결 (@ObservableObject > @ObservedObject)

--- 15-in-practice-combine-and-swiftui/projects/starter/HNReader/View/ReaderView.swift
+++ 15-in-practice-combine-and-swiftui/projects/starter/HNReader/View/ReaderView.swift
@@ -27,65 +27,70 @@
 /// THE SOFTWARE.
 
 import SwiftUI
-import Combine
 
 struct ReaderView: View {
-  @ObservableObject var model = ReaderViewModel() /// 웨않뒈 .. (에러): Unknown attribute 'ObservableObject'
-  @State var presentingSettingsSheet = false
-
-  var currentDate = Date()
-  
-  init(model: ReaderViewModel) {
-    self.model = model
-  }
-  
-  var body: some View {
-    let filter = "Showing all stories"
     
-    return NavigationView {
-      List {
-        Section(header: Text(filter).padding(.leading, -10)) {
-          ForEach(self.model.stories) { story in
-            VStack(alignment: .leading, spacing: 10) {
-              TimeBadge(time: story.time)
-              
-              Text(story.title)
-                .frame(minHeight: 0, maxHeight: 100)
-                .font(.title)
-              
-              PostedBy(time: story.time, user: story.by, currentDate: self.currentDate)
-              
-              Button(story.url) {
-                print(story)
-              }
-              .font(.subheadline)
-              .foregroundColor(Color.blue)
-              .padding(.top, 6)
-            }
-            .padding()
-          }
-          // Add timer here
-        }.padding()
-      }
-      .listStyle(PlainListStyle())
-      .sheet(isPresented: self.$presentingSettingsSheet, // $변수명 = 해당 변수값이 변하는걸 감지
-             content: { SettingsView() })
-      // Display errors here
-      .navigationBarTitle(Text("\(self.model.stories.count) Stories"))
-      .navigationBarItems(trailing:
-        Button("Settings") {
-          self.presentingSettingsSheet = true // Settings 버튼을 누르면 presentingSettingsSheet을 true로 바꿈
-          
-        }
-      )
+    @ObservedObject var model: ReaderViewModel
+    @State var presentingSettingsSheet = false
+    
+    var currentDate = Date()
+    
+    init(model: ReaderViewModel) {
+        self.model = model
     }
-  }
+    
+    var body: some View {
+        let filter = "Showing all stories"
+        
+        return NavigationView {
+            List {
+                Section(header: Text(filter).padding(.leading, -10)) {
+                    ForEach(self.model.stories) { story in
+                        VStack(alignment: .leading, spacing: 10) {
+                            TimeBadge(time: story.time)
+                            
+                            Text(story.title)
+                                .frame(minHeight: 0, maxHeight: 100)
+                                .font(.title)
+                            
+                            PostedBy(time: story.time, user: story.by, currentDate: self.currentDate)
+                            
+                            Button(story.url) {
+                                print(story)
+                            }
+                            .font(.subheadline)
+                            .foregroundColor(Color.blue)
+                            .padding(.top, 6)
+                        }
+                        .padding()
+                    }
+                    // Add timer here
+                }.padding()
+            }
+            .listStyle(PlainListStyle())
+            .sheet(isPresented: self.$presentingSettingsSheet, // $변수명 = 해당 변수값이 변하는걸 감지
+                   content: { SettingsView() })
+            .alert(item: self.$model.error, content: { error in 
+                Alert(title: Text("Network error"),
+                      message: Text(error.localizedDescription),
+                      dismissButton: .cancel()
+                )
+            })
+            .navigationBarTitle(Text("\(self.model.stories.count) Stories"))
+            .navigationBarItems(trailing:
+                                    Button("Settings") {
+                self.presentingSettingsSheet = true // Settings 버튼을 누르면 presentingSettingsSheet을 true로 바꿈
+                
+            }
+            )
+        }
+    }
 }
 
 #if DEBUG
 struct ReaderView_Previews: PreviewProvider {
-  static var previews: some View {
-    ReaderView(model: ReaderViewModel())
-  }
+    static var previews: some View {
+        ReaderView(model: ReaderViewModel())
+    }
 }
 #endif
