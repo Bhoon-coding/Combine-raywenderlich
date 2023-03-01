9769dcf6039810e395ad915e3136b15ccb0e4808
bhoon-coding <bhooncoding@gmail.com>
2023-03-01 18:43:08 +0900

    [Combine] Chapter 16: Designing your fallible APIs

--- 16-error-handling/projects/Starter.playground/Pages/Designing your fallible APIs.xcplaygroundpage/Contents.swift
+++ 16-error-handling/projects/Starter.playground/Pages/Designing your fallible APIs.xcplaygroundpage/Contents.swift
@@ -3,8 +3,102 @@ import Foundation
 import Combine
 
 var subscriptions = Set<AnyCancellable>()
-//: ## Designing your fallible APIs
-<#Add your code here#>
+
+example(of: "Joke API") {
+    
+    // MARK: - 에러가 발생할 수 있는 상황
+    /// 1. dataTaskPublisher에서 URLError (연결문제, 유효하지않은 요청 등)
+    /// 2. jokeID가 없을경우
+    /// 3. JSON 디코딩이 실패 했을 경우
+    /// 4. 알수없는 에러
+    
+    class DadJokes {
+        
+        // 1
+        struct Joke: Codable {
+            let id: String
+            let joke: String
+        }
+        
+        enum Error: Swift.Error, CustomStringConvertible {
+            // 1
+            case network
+            case jokeDoesntExist(id: String)
+            case parsing
+            case unknown
+            
+            // 2
+            var description: String {
+                switch self {
+                case .network:
+                    return "Request to API Server failed"
+                case .parsing:
+                    return "Failed parsing response from server"
+                case .jokeDoesntExist(let id):
+                    return "Joke with ID \(id) doesn't exist"
+                case .unknown:
+                    return "An unknown error occrred"
+                }
+            }
+        }
+        
+        // 2
+        func getJoke(id: String) -> AnyPublisher<Joke, Error> {
+            // id가 최소 한 글자라도 있어야함.
+            guard id.rangeOfCharacter(from: .letters) != nil else {
+                return Fail<Joke, Error>(
+                    error: .jokeDoesntExist(id: id)
+                )
+                .eraseToAnyPublisher()
+            }
+            let url = URL(string: "https://icanhazdadjoke.com/j/\(id)")!
+            var request = URLRequest(url: url)
+            /// request.allHTTPHeaderFields 가 없으면 → failure(Failed parsing response from server)
+            request.allHTTPHeaderFields = ["Accept": "application/json"]
+
+            
+        // 3
+            return URLSession.shared
+                .dataTaskPublisher(for: request)
+//                .map(\.data)
+                .tryMap { data, _ -> Data in
+                    // 6
+                    guard let obj = try? JSONSerialization.jsonObject(with: data),
+                          let dict = obj as? [String: Any],
+                          dict["status"] as? Int == 404 else {
+                        return data
+                    }
+                    // 7
+                    throw DadJokes.Error.jokeDoesntExist(id: id)
+                }
+                .decode(type: Joke.self, decoder: JSONDecoder())
+                .mapError { error -> DadJokes.Error in
+                    switch error {
+                    case is URLError:
+                        return .network
+                    case is DecodingError:
+                        return .parsing
+                    default:
+                        return error as? DadJokes.Error ?? .unknown
+                    }
+                }
+                .eraseToAnyPublisher()
+        }
+    }
+    
+    // 4
+    let api = DadJokes()
+    let jokeID = "9prWnjyImyd"
+    let badJokeID = "123456"
+    
+    // 5
+    api
+        .getJoke(id: jokeID)
+//        .getJoke(id: badJokeID) // 에러발생
+        .sink(receiveCompletion: { print($0) },
+              receiveValue: { print("God joke: \($0)") })
+        .store(in: &subscriptions)
+}
 //: [Next](@next)
 
 /// Copyright (c) 2021 Razeware LLC
