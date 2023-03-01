a648519e53e9b0edc6d813929ee2e520341ebfe9
bhoon-coding <bhooncoding@gmail.com>
2023-03-01 16:30:12 +0900

    [Combine] Chapter 16: try-operator

--- 16-error-handling/projects/Starter.playground/Pages/try* operators.xcplaygroundpage/Contents.swift
+++ 16-error-handling/projects/Starter.playground/Pages/try* operators.xcplaygroundpage/Contents.swift
@@ -6,24 +6,35 @@ var subscriptions = Set<AnyCancellable>()
 
 //: ## try* operators
 example(of: "tryMap") {
-    // 1
     enum NameError: Error {
         case tooShort(String)
         case unowned
     }
     
-    // 2
     let names = ["Marin", "Shai", "Florent"].publisher
     
-    names
-    // 3
-        .map { value in
+        names
+        .tryMap { value -> Int in
+            // 1
+            let length = value.count
+            
+            // 2
+            guard length >= 5 else {
+                throw NameError.tooShort(value)
+            }
+            
+            // 3
             return value.count
         }
         .sink(receiveCompletion: { print("Completed with \($0)") },
               receiveValue: { print("Got value: \($0)") }
         )
 }
+
+//——— Example of: tryMap ———
+//Got value: 5
+//Completed with failure(...NameError.tooShort("Shai"))
+
 
 //: [Next](@next)
 
