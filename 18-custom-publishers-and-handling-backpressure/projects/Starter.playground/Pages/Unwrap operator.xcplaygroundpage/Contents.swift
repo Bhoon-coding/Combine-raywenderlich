0a610590b32e0c7fba1b0f360da2a697782799b2
bhoon-coding <bhooncoding@gmail.com>
2023-03-19 14:29:01 +0900

    [Combine] Chapter 18: Publihsers extension methods 만들어보기 (unwrap())

--- 18-custom-publishers-and-handling-backpressure/projects/Starter.playground/Pages/Unwrap operator.xcplaygroundpage/Contents.swift
+++ 18-custom-publishers-and-handling-backpressure/projects/Starter.playground/Pages/Unwrap operator.xcplaygroundpage/Contents.swift
@@ -1,6 +1,25 @@
 import Combine
 
-<#Add your code here#>
+extension Publisher {
+    // 1
+    func unwrap<T>() -> Publishers.CompactMap<Self, T> where Output == Optional<T> {
+        // 2
+        compactMap { $0 }
+    }
+}
+
+let values: [Int?] = [1, 2, nil, 3, nil, 4]
+
+values.publisher
+    .unwrap()
+    .sink {
+        print("Received value: \($0)")
+    }
+
+//Received value: 1
+//Received value: 2
+//Received value: 3
+//Received value: 4
 
 //: [Next](@next)
 /*:
