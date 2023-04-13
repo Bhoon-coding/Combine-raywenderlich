8109fb6f492d6e1f38bfa49cf76858f3a2093c3f
bhoon-coding <bhooncoding@gmail.com>
2023-04-13 10:19:51 +0900

    [Combine] Chapter 20: x좌표에 따라 JokeCard 색상 변경 (updateDecisionStateForChange, updateBackgroundColor) 구현

--- 20-in-practice-building-a-complete-app/projects/starter/ChuckNorrisJokesModel/View Models/JokesViewModel.swift
+++ 20-in-practice-building-a-complete-app/projects/starter/ChuckNorrisJokesModel/View Models/JokesViewModel.swift
@@ -34,7 +34,7 @@ import UIKit
 import Combine
 import SwiftUI
 
-public final class JokesViewModel {
+public final class JokesViewModel: ObservableObject {
     public enum DecisionState {
         case disliked, undecided, liked
     }
